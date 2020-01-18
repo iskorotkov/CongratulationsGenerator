51f1cd514b2101c424fae4dea5f1f6fff72f9134
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-01-18 11:06:50 +0500

    Add inserting new lists when adding new recipients

--- CongratulationsGenerator.MicrosoftOffice/WordDoc.cs
+++ CongratulationsGenerator.MicrosoftOffice/WordDoc.cs
@@ -6,25 +6,42 @@ namespace CongratulationsGenerator.MicrosoftOffice
 {
     public class WordDoc : ITemplateDocument
     {
+        private readonly string _filename;
         private readonly Application _app;
         private readonly Document _doc;
+
+        private bool _shouldAddTemplateList;
         
         public WordDoc(string filename)
         {
             _app = new Application();
             _doc = _app.Documents.Add(filename);
+            _filename = filename;
+        }
+
+        private void AddTemplateList()
+        {
+            _app.Selection.EndKey(WdUnits.wdStory);
+            _app.Selection.InsertNewPage();
+            _app.Selection.InsertFile(_filename, "", false, false, false);
         }
 
         public void AddRecipient(Recipient recipient, IEnumerable<string> wishes)
         {
+            if (_shouldAddTemplateList)
+            {
+                AddTemplateList();
+            }
+
+            _shouldAddTemplateList = true;
+            
             _doc.Bookmarks["Dear"].Range.Text = recipient.Gender.DearForm;
             _doc.Bookmarks["Name"].Range.Text = recipient.Name;
 
-            var index = 0;
+            var index = 1;
             foreach (var wish in wishes)
             {
                 _doc.Bookmarks["Wish" + index].Range.Text = wish;
-                
                 index++;
             }
         }
