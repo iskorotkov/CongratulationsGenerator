b83ff6a8135206f278d10ac0ae02ae287489a977
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-01-18 19:11:45 +0500

    Refactor TripleGenerator.cs

--- CongratulationsGenerator.WishesDistributors/TripleGenerator.cs
+++ CongratulationsGenerator.WishesDistributors/TripleGenerator.cs
@@ -6,79 +6,122 @@ namespace CongratulationsGenerator.WishesDistributors
 {
     public class TripleGenerator
     {
-        private List<int> _wishesUsedPerCategory;
+        private IReadOnlyList<WishCategory> _wishes;
+        private List<int> _usedWishesPerCategory;
         public List<Triple> OptimalVariants { get; private set; } = new List<Triple>();
         public List<Triple> OtherVariants { get; private set; } = new List<Triple>();
 
         public void GenerateTriples(IReadOnlyList<WishCategory> wishes, IPermutationGenerator permutationGenerator)
         {
-            _wishesUsedPerCategory = new List<int>(wishes.Count);
-            for (var i = 0; i < wishes.Count; i++)
-            {
-                _wishesUsedPerCategory.Add(0);
-            }
-
-            IterateOverCategories(wishes);
-            
+            _wishes = wishes;
+            InitializeUsedWishesList();
+            IterateOverCategories();
             OptimalVariants = permutationGenerator.MakePermutation(OptimalVariants).ToList();
             OtherVariants = permutationGenerator.MakePermutation(OtherVariants).ToList();
         }
 
-        private void IterateOverCategories(IReadOnlyList<WishCategory> wishes)
+        private void InitializeUsedWishesList()
         {
-            var length = wishes.Count;
+            _usedWishesPerCategory = new List<int>(_wishes.Count);
+            for (var i = 0; i < _wishes.Count; i++)
+            {
+                _usedWishesPerCategory.Add(0);
+            }
+        }
+
+        private void IterateOverCategories()
+        {
+            var length = _wishes.Count;
             for (var firstCategory = 0; firstCategory < length; firstCategory++)
             {
                 for (var secondCategory = firstCategory + 1; secondCategory < length; secondCategory++)
                 {
                     for (var thirdCategory = secondCategory + 1; thirdCategory < length; thirdCategory++)
                     {
-                        ForEveryCategoryTriple(wishes, firstCategory, secondCategory, thirdCategory);
+                        ForEveryCategoryTriple(firstCategory, secondCategory, thirdCategory);
                     }
                 }
             }
         }
 
-        private void ForEveryCategoryTriple(IReadOnlyList<WishCategory> wishes, int firstCategory, int secondCategory,
-            int thirdCategory)
+        private void ForEveryCategoryTriple(int firstCategory, int secondCategory, int thirdCategory)
         {
-            for (var firstWish = 0; firstWish < wishes[firstCategory].Wishes.Count; firstWish++)
+            for (var firstWish = 0; firstWish < Wishes(firstCategory).Count; firstWish++)
             {
-                for (var secondWish = 0; secondWish < wishes[secondCategory].Wishes.Count; secondWish++)
+                for (var secondWish = 0; secondWish < Wishes(secondCategory).Count; secondWish++)
                 {
-                    for (var thirdWish = 0; thirdWish < wishes[thirdCategory].Wishes.Count; thirdWish++)
+                    for (var thirdWish = 0; thirdWish < Wishes(thirdCategory).Count; thirdWish++)
                     {
-                        ForEveryWishTriple(wishes, 
-                            firstCategory, firstWish, 
+                        ForEveryWishTriple(
+                            firstCategory, firstWish,
                             secondCategory, secondWish,
-                            thirdCategory, thirdWish);
+                            thirdCategory, thirdWish
+                        );
                     }
                 }
             }
         }
 
-        private void ForEveryWishTriple(IReadOnlyList<WishCategory> wishes,
+        private List<string> Wishes(int category)
+        {
+            return _wishes[category].Wishes;
+        }
+
+        private void ForEveryWishTriple(
             int firstCategory, int firstWish,
             int secondCategory, int secondWish,
-            int thirdCategory, int thirdWish)
+            int thirdCategory, int thirdWish
+        )
         {
-            var collectionToAdd = OtherVariants;
-            if (firstWish >= _wishesUsedPerCategory[firstCategory]
-                && secondWish >= _wishesUsedPerCategory[secondCategory]
-                && thirdWish >= _wishesUsedPerCategory[thirdCategory])
-            {
-                collectionToAdd = OptimalVariants;
+            var collectionToAdd = CollectionToAdd(
+                firstCategory, firstWish,
+                secondCategory, secondWish,
+                thirdCategory, thirdWish
+            );
 
-                _wishesUsedPerCategory[firstCategory]++;
-                _wishesUsedPerCategory[secondCategory]++;
-                _wishesUsedPerCategory[thirdCategory]++;
-            }
-
-            var first = wishes[firstCategory].Wishes[firstWish];
-            var second = wishes[secondCategory].Wishes[secondWish];
-            var third = wishes[thirdCategory].Wishes[thirdWish];
+            var first = Wish(firstCategory, firstWish);
+            var second = Wish(secondCategory, secondWish);
+            var third = Wish(thirdCategory, thirdWish);
 
             collectionToAdd.Add(new Triple(first, second, third));
+        }
+
+        private List<Triple> CollectionToAdd(
+            int firstCategory, int firstWish,
+            int secondCategory, int secondWish,
+            int thirdCategory, int thirdWish
+        )
+        {
+            if (!AreWishesAvailable(firstCategory, firstWish, secondCategory, secondWish, thirdCategory, thirdWish))
+            {
+                return OtherVariants;
+            }
+
+            MarkWishesAsTaken(firstCategory, secondCategory, thirdCategory);
+            return OptimalVariants;
+        }
+
+        private string Wish(int category, int wish)
+        {
+            return _wishes[category].Wishes[wish];
+        }
+
+        private void MarkWishesAsTaken(int firstCategory, int secondCategory, int thirdCategory)
+        {
+            _usedWishesPerCategory[firstCategory]++;
+            _usedWishesPerCategory[secondCategory]++;
+            _usedWishesPerCategory[thirdCategory]++;
+        }
+
+        private bool AreWishesAvailable(
+            int firstCategory, int firstWish,
+            int secondCategory, int secondWish,
+            int thirdCategory, int thirdWish
+        )
+        {
+            return firstWish >= _usedWishesPerCategory[firstCategory]
+                   && secondWish >= _usedWishesPerCategory[secondCategory]
+                   && thirdWish >= _usedWishesPerCategory[thirdCategory];
         }
     }
 }
