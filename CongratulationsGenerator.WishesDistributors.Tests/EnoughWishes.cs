de680d6192cbb54c5ec359cf16aa646d46c992d8
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-01-19 18:57:56 +0500

    Add tests for checking whether there are enough wishes

---
+++ CongratulationsGenerator.WishesDistributors.Tests/EnoughWishes.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using CongratulationsGenerator.Core;
+using NUnit.Framework;
+
+namespace CongratulationsGenerator.WishesDistributors.Tests
+{
+    [TestFixture]
+    public class EnoughWishes
+    {
+        [Test]
+        public void UpToThreeCategories()
+        {
+            var distributor = new Distributor(CreateWishes(new[] {1}));
+            Assert.IsTrue(distributor.IsEnoughWishes(0));
+            Assert.IsFalse(distributor.IsEnoughWishes(1));
+            
+            distributor = new Distributor(CreateWishes(new[] {2, 2}));
+            Assert.IsFalse(distributor.IsEnoughWishes(1));
+
+            distributor = new Distributor(CreateWishes(new[] {2, 2, 1}));
+            Assert.IsTrue(distributor.IsEnoughWishes(1));
+
+            distributor = new Distributor(CreateWishes(new[] {2, 2, 1}));
+            Assert.IsTrue(distributor.IsEnoughWishes(4));
+            Assert.IsFalse(distributor.IsEnoughWishes(5));
+        }
+
+        [Test]
+        public void MoreCategories()
+        {
+            var distributor = new Distributor(CreateWishes(new[] {2, 2, 2, 2}));
+            Assert.IsTrue(distributor.IsEnoughWishes(0));
+            Assert.IsTrue(distributor.IsEnoughWishes(32));
+            Assert.IsFalse(distributor.IsEnoughWishes(33));
+        }
+
+        private static IEnumerable<WishCategory> CreateWishes(IEnumerable<int> wishesPerCategory)
+        {
+            var results = new List<WishCategory>();
+            foreach (var n in wishesPerCategory)
+            {
+                var wishes = new List<string>(n);
+                for (var i = 0; i < n; i++)
+                {
+                    wishes.Add("Wish");
+                }
+                
+                results.Add(new WishCategory("Name", wishes));
+            }
+
+            return results;
+        }
+    }
+}
